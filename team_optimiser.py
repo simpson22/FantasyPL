d3a9d8a53e321e05e205c54ba6526d82f70f0690
simpson22 <josholiversimpson@hotmail.com>
2019-02-11 17:49:06 +0000

    Added some commenting

--- team_optimiser.py
+++ team_optimiser.py
@@ -6,6 +6,7 @@ import player as pl
 
 myTeam = pl.u_PlayerListArray
 
+# Allows the user to filter the candidate pool before solving
 while True:
     print(len(myTeam), 'Candidates remaining\nWould you like to filter the candidate pool y/n')
     answer = input()
@@ -22,6 +23,7 @@ playerDEF = {}
 playerMID = {}
 playerFWD = {}
 
+# Creates player attribute arrays
 for players in myTeam:
     playerIds.append(players['id'])
     playerPoints[players['id']] = players['total_points']
@@ -41,6 +43,7 @@ for players in myTeam:
 
 allTeamArray = {i: 0 for i in range(1, 21)}
 
+# Creates player team attribute arrays
 for team in allTeamArray:
     playerTeam = {}
     for players in myTeam:
@@ -51,26 +54,28 @@ for team in allTeamArray:
     allTeamArray[team] = playerTeam
 
 problem = LpProblem('The FPL Team Problem', LpMaximize)
-playerVariables = LpVariable.dicts("Player", playerIds, 0, 1, LpInteger)
+playerVariables = LpVariable.dicts("Player", playerIds, 0, 1, LpInteger)  # Player variables "Player_1" Binary value
 
-problem += lpSum([playerPoints[i]*playerVariables[i] for i in playerIds])
+problem += lpSum([playerPoints[i]*playerVariables[i] for i in playerIds])  # Player scores
 
-problem += lpSum([playerGKP[i]*playerVariables[i] for i in playerIds]) == 1
-problem += lpSum([playerDEF[i]*playerVariables[i] for i in playerIds]) >= 3
-problem += lpSum([playerDEF[i]*playerVariables[i] for i in playerIds]) <= 5
-problem += lpSum([playerMID[i]*playerVariables[i] for i in playerIds]) >= 3
-problem += lpSum([playerMID[i]*playerVariables[i] for i in playerIds]) <= 5
-problem += lpSum([playerFWD[i]*playerVariables[i] for i in playerIds]) >= 1
-problem += lpSum([playerFWD[i]*playerVariables[i] for i in playerIds]) <= 3
+problem += lpSum([playerGKP[i]*playerVariables[i] for i in playerIds]) == 1  # Only 1 Goalkeeper
+problem += lpSum([playerDEF[i]*playerVariables[i] for i in playerIds]) >= 3  # More than or equal to 3 Defenders
+problem += lpSum([playerDEF[i]*playerVariables[i] for i in playerIds]) <= 5  # Less than or equal to 5 Defenders
+problem += lpSum([playerMID[i]*playerVariables[i] for i in playerIds]) >= 3  # More than or equal to 3 Midfielders
+problem += lpSum([playerMID[i]*playerVariables[i] for i in playerIds]) <= 5  # Less than or equal to 5 Midfielders
+problem += lpSum([playerFWD[i]*playerVariables[i] for i in playerIds]) >= 1  # More than or equal to 1 Striker
+problem += lpSum([playerFWD[i]*playerVariables[i] for i in playerIds]) <= 3  # Less than or equal to 3 Strikers
 
+# Less than or equal to 3 players per team
 for team in allTeamArray:
     teamArray = allTeamArray[team]
     problem += lpSum([teamArray[i] * playerVariables[i] for i in playerIds]) <= 3
 
-problem += lpSum([playerCost[i]*playerVariables[i] for i in playerIds]) <= 850
+problem += lpSum([playerCost[i]*playerVariables[i] for i in playerIds]) <= 850  # Less than or equal to 85M budget
 
-problem += lpSum([playerVariables[i] for i in playerIds]) == 11
+problem += lpSum([playerVariables[i] for i in playerIds]) == 11  # 11 Players
 
+# Creates the lp problem file and solves the problem
 problem.writeLP("FPL_Team_Problem.lp")
 problem.solve()
 
