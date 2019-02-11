83aba9472b1c35f1b8c4709dd0967411c28dbf7b
simpson22 <josholiversimpson@hotmail.com>
2019-02-11 17:41:05 +0000

    Added team constraints, maximum 3 players per team

--- team_optimiser.py
+++ team_optimiser.py
@@ -1,18 +1,10 @@
-"""The simplified team selection model using PuLP"""
+"""The Fantasy team selection model using PuLP"""
 
 from pulp import *
 import team_selector as ts
 import player as pl
 
 myTeam = pl.u_PlayerListArray
-playerIds = []
-playerPoints = {}
-playerCost = {}
-playerGKP = {}
-playerDEF = {}
-playerMID = {}
-playerFWD = {}
-playerVariables = {}
 
 while True:
     print(len(myTeam), 'Candidates remaining\nWould you like to filter the candidate pool y/n')
@@ -21,6 +13,14 @@ while True:
         myTeam = ts.filter_players(pl.u_PlayerListArray)
     else:
         break
+
+playerIds = []
+playerPoints = {}
+playerCost = {}
+playerGKP = {}
+playerDEF = {}
+playerMID = {}
+playerFWD = {}
 
 for players in myTeam:
     playerIds.append(players['id'])
@@ -39,6 +39,16 @@ for players in myTeam:
     else:
         playerFWD[players['id']] = 1
 
+allTeamArray = {i: 0 for i in range(1, 21)}
+
+for team in allTeamArray:
+    playerTeam = {}
+    for players in myTeam:
+        if players['team_code'] == team:
+            playerTeam[players['id']] = 1
+        else:
+            playerTeam[players['id']] = 0
+    allTeamArray[team] = playerTeam
 
 problem = LpProblem('The FPL Team Problem', LpMaximize)
 playerVariables = LpVariable.dicts("Player", playerIds, 0, 1, LpInteger)
@@ -53,12 +63,15 @@ problem += lpSum([playerMID[i]*playerVariables[i] for i in playerIds]) <= 5
 problem += lpSum([playerFWD[i]*playerVariables[i] for i in playerIds]) >= 1
 problem += lpSum([playerFWD[i]*playerVariables[i] for i in playerIds]) <= 3
 
+for team in allTeamArray:
+    teamArray = allTeamArray[team]
+    problem += lpSum([teamArray[i] * playerVariables[i] for i in playerIds]) <= 3
+
 problem += lpSum([playerCost[i]*playerVariables[i] for i in playerIds]) <= 850
 
 problem += lpSum([playerVariables[i] for i in playerIds]) == 11
 
 problem.writeLP("FPL_Team_Problem.lp")
-
 problem.solve()
 
 print("Status:", LpStatus[problem.status])
