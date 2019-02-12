29415fe0dddc3543ed3533912b44250bd923448f
simpson22 <josholiversimpson@hotmail.com>
2019-02-12 23:30:55 +0000

    Defined the scraper, to access more FPL Api based on a mode input

--- scraper.py
+++ scraper.py
@@ -1,26 +1,46 @@
+"""The web scraper for ttps://fantasy.premierleague.com/drf API"""
 import requests
 import json
 
-# Try to download fpl static data, print response code and any error
-try:
-    requestData = requests.get('https://fantasy.premierleague.com/drf/bootstrap-static')
-    requestData.raise_for_status()
-except requests.exceptions.RequestException as e:
-    print('Could not request data:', e, sep='\n')
-    success = 0
-else:
-    # Open and write to a raw_data.json file, dumping request object as json
+modes = dict(raw_data='bootstrap-static',
+             player='element-summary',
+             fixtures='fixtures', )
+
+
+def fantasy_request(mode='bootstrap-static', element=None):
+
+    if element is not None:
+        file_element = element
+        element = '/' + element
+    else:
+        element = ''
+        file_element = ''
     try:
-        with open('data\\raw_data.json', 'w') as write_file:
-            json.dump(requestData.json(), write_file)
-    except EnvironmentError as e:
-        print('Could not write to file', e, sep='\n')
+        request_data = requests.get('https://fantasy.premierleague.com/drf/' + modes[mode] + element)
+        request_data.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print('Could not request data:', e, sep='\n')
         success = 0
     else:
-        success = 1
+        # Open and write to a raw_data.json file, dumping request object as json
+        try:
+            with open('data\\' + mode + '\\' + mode + file_element + '.json', 'w') as write_file:
+                json.dump(request_data.json(), write_file)
+        except EnvironmentError as e:
+            print('Could not write ' + mode + ' file', e, sep='\n')
+            success = 0
+        else:
+            print(mode + ' successfully extracted')
+            success = 1
+    return success
+
 
 if __name__ == '__main__':
-    if success:
+    print('Please enter one of the following modes')
+    print(modes)
+    kwargs = [str(x) for x in input().split()]
+    status = fantasy_request(*kwargs)
+    if status:
         print('scraper ran successfully')
     else:
         print('scraper ran with errors')
