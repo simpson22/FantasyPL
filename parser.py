d7cef931eec8693c18e2a6aedbe58a1d85755190
simpson22 <josholiversimpson@hotmail.com>
2019-02-13 23:13:28 +0000

    Refactoring parser

--- parser.py
+++ parser.py
@@ -1,17 +1,38 @@
 import json
+import scraper as sc
 
-# Open the raw_data.json in read only, json format, and initialise list
-fplData = json.load(open('data\\raw_data.json', 'r'))
-fplObjectList = []
+fileLocations = dict(sc.modes)
+fileLocations['raw_data'] = 'data\\raw_data\\raw_data.json'
+fileLocations['fixtures'] = 'data\\fixtures\\fixtures.json'
+fileLocations['player'] = 'data\\player\\player1.json'
 
-# Create a list of all the object types in fpl data
-for fplObject in fplData:
-    fplObjectList.append(str(fplObject))
 
-# For each object type write the data to a new file
-for filename in fplObjectList:
-    with open('data\\' + filename + '.json', 'w') as write_file:
-        json.dump(fplData[filename], write_file)
+def read_json_data(file_location):
+    """Opens file containing json data and returns json object."""
+    json_data = json.load(open(file_location, 'r'))
+    return json_data
+
+
+def find_json_objects(json_data):
+    """Iterates over data and returns a list of object names."""
+    object_list = []
+    for json_object in json_data:
+        object_list.append(str(json_object))
+    return object_list
+
+
+def show_first_json_item(fpl_object, json_data):
+    """Find the first item in the fpl object and returns it."""
+    json_series = json_data[fpl_object]
+    json_item = json_series[0]
+    return json_item
+
 
 if __name__ == '__main__':
+    print('Select a file to analyse\n' + str(fileLocations.keys()))
+    fplData = read_json_data(fileLocations[input()])
+    fplObjects = find_json_objects(fplData)
+    print('Select an object to inspect\n' + str(fplObjects))
+    fplItem = show_first_json_item(input(), fplData)
+    print(fplItem)
     print('parser ran successfully')
--- scraper.py
+++ scraper.py
@@ -8,6 +8,7 @@ modes = dict(raw_data='bootstrap-static',
 
 
 def fantasy_request(mode='bootstrap-static', element=''):
+    """Provide a mode, and element if applicable, requests the data from FPL API, return success code and data."""
     data = None
     if element is not '':
         element = '/' + element
@@ -23,7 +24,9 @@ def fantasy_request(mode='bootstrap-static', element=''):
     return success, data
 
 
-def write_file(mode, element='', write_data=None):
+def write_json_file(mode, element='', write_data=None):
+    """Provide a mode and element if applicable, writes the FPL API json format to a standard file location
+     returning status"""
     file_location = 'data\\' + mode + '\\' + mode + element + '.json'
     try:
         with open(file_location, 'w') as file:
@@ -42,7 +45,7 @@ if __name__ == '__main__':
     print(modes)
     kwargs = [str(x) for x in input().split()]
     request_status, request_data = fantasy_request(*kwargs)
-    write_status = write_file(*kwargs, write_data=request_data)
+    write_status = write_json_file(*kwargs, write_data=request_data)
     if request_status and write_status:
         print('scraper ran successfully')
     else:
