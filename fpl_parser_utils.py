5cf67280e6844d55f5a7fdad35c3262b4cf0db4e
simpson22 <josholiversimpson@hotmail.com>
2019-02-23 12:58:35 +0000

    Refactor parser, with list comprehension, some docstrings and handling of more json types in show_json_item

--- fpl_parser_utils.py
+++ fpl_parser_utils.py
@@ -3,17 +3,27 @@ import os
 
 
 def find_read_modes():
-    """Walks the data directory and returns mode, filepath dictionary"""
+    """Find a file to filepath mapping in the data directory.
+
+    Returns:
+        Dictionary containing file : filepath mapping"""
     read_modes = {}
     for folder, subFolders, files in os.walk('data'):
-        for file in files:
-            filename = file.split('.')
-            read_modes[filename[0]] = folder + '\\' + file
+        current_modes = {(file.split('.')[0]): '{}\\{}'.format(folder, file) for file in files}
+        read_modes.update(current_modes)
     return read_modes
 
 
 def read_json_data(file_location):
-    """Opens file containing json data and returns json object."""
+    """Opens file and returns json object
+
+    Args:
+        Filename or direct Filepath string
+
+    Returns:
+        Json object of the data from file."""
+    if '.' not in file_location:
+        file_location = readLocations[file_location]
     json_data = json.load(open(file_location, 'r'))
     return json_data
 
@@ -21,28 +31,35 @@ def read_json_data(file_location):
 def find_json_objects(json_data):
     """Iterates over data and returns a list of object names, if not a list of objects return first element"""
     object_list = []
-    is_object = 0
     try:
         for json_object in json_data:
             if type(json_object) is str:
                 object_list.append(json_object)
-                is_object = 1
             else:
-                print('Already at object level, first object is: \n' + str(json_data[0]))
+                print('Already at object level, first object is: \n{}'.format(str(json_data[0])))
+                object_list = [x['id'] for x in json_data]
                 break
     except TypeError as e:
-        print('Could not find json objects',  e, sep='\n')
-        object_list = json_data
-    return object_list, is_object
+        print('Could not find iterable json object\njson_data: {}\n{}'.format(json_data, e))
+    return object_list
 
 
 def show_json_item(fpl_object, json_data):
     """Find the first item in the fpl object and returns it."""
     try:
-        json_item = json_data[0]
-    except KeyError:
+        json_item = json_data[int(fpl_object) - 1]
+        print('Object id {} returned'.format(fpl_object))
+    except ValueError:
         json_series = json_data[fpl_object]
-        json_item = json_series[0]
+        try:
+            json_item = json_series[0]
+            print('First item from {} returned'.format(fpl_object))
+        except KeyError:
+            json_item = json_series
+            print('Nested dictionary found')
+        except TypeError:
+            json_item = json_series
+            print('Un-Iterable object found')
     return json_item
 
 
@@ -50,9 +67,9 @@ readLocations = find_read_modes()
 
 if __name__ == '__main__':
     print('Select a file to analyse\n' + str(readLocations.keys()))
-    fplData = read_json_data(readLocations[input()])
-    fplObjects, isObjects = find_json_objects(fplData)
-    if isObjects == 1:
+    fplData = read_json_data(input())
+    fplObjects = find_json_objects(fplData)
+    if fplObjects:
         print('Select an object to inspect\n' + str(fplObjects))
         fplItem = show_json_item(input(), fplData)
         print(fplItem)
