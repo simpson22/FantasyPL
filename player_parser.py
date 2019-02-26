61b38ccf023701a03e3b7951c5fdd847640e047e
simpson22 <josholiversimpson@hotmail.com>
2019-02-26 00:02:01 +0000

    Completed individual player points analysis including plots.

--- player_parser.py
+++ player_parser.py
@@ -1,11 +1,12 @@
 """Requests and Writes all Individual player data from  https://fantasy.premierleague.com/"""
 
-import fpl_scraper_utils as fs
-import fpl_parser_utils as fp
+import fpl_scraper_utils as su
+import fpl_parser_utils as pu
+import matplotlib.pyplot as plt
 
 # Read fixture data, gather difficulty ratings for all fixtures
 readMode = 'fixtures'
-fixtureData = fp.read_json_data(fp.readLocations[readMode])
+fixtureData = pu.read_json_data(pu.readLocations[readMode])
 
 interestedFixtureKeys = ['id', 'team_h_difficulty', 'team_a_difficulty']
 fixtureList = []
@@ -21,12 +22,12 @@ requestMode = 'player'
 elements = list(range(115, 116))
 
 for element in elements:
-    requestData = fs.fantasy_request(requestMode, element)
-    fs.write_json_file(requestMode, element, write_data=requestData)
+    requestData = su.fantasy_request(requestMode, element)
+    su.write_json_file(requestData, requestMode, element)
 
 readMode = requestMode + str(elements[0])
 
-playerData = fp.read_json_data(fp.readLocations[readMode])
+playerData = pu.read_json_data(pu.readLocations[readMode])
 
 interestedHistoryKeys = ['element',
                          'round',
@@ -36,7 +37,7 @@ interestedHistoryKeys = ['element',
 playerHistoryList = []
 playerHistory = {}
 
-# Create list array for each fixture played, gathering key statistics and finding the difficulty fixture
+# Create list array for each fixture played, gathering key statistics and finding the difficulty fixture.
 for history_element in playerData['history']:
     for key in interestedHistoryKeys:
         playerHistory[key] = history_element[key]
@@ -48,10 +49,36 @@ for history_element in playerData['history']:
                 playerHistory['difficulty'] = fixture['team_a_difficulty']
     playerHistoryList.append(dict(playerHistory))
 
+# Create total, round, history and last3 point statistics.
+for game_round in playerHistoryList:
+    game_round['round_points'] = game_round['total_points']
+    game_round['history_points'] = \
+        sum([int(x['round_points']) for x in playerHistoryList if x['round'] < game_round['round']])
+    game_round['weighted_history_points'] = \
+        round(sum([int(x['round_points']) for x in playerHistoryList if x['round'] < game_round['round']])
+                        / game_round['round'], 2)
+    game_round['total_points'] = game_round['round_points'] + game_round['history_points']
+    game_round['last3_points'] = sum([int(x['round_points']) for
+                            x in playerHistoryList if game_round['round'] - 4 < x['round'] < game_round['round']])
+
 print(playerHistoryList)
 
+round_points = []
+last3_points = []
+weighted_history_points = []
+for game_round in playerHistoryList:
+    if game_round['round'] > 3:
+        round_points.append(game_round['round_points'])
+        last3_points.append(game_round['last3_points'] / 3)
+        weighted_history_points.append(game_round['weighted_history_points'])
+
+plt.scatter(last3_points, round_points, color='r')
+plt.scatter(weighted_history_points, round_points, color='b')
+plt.xlabel('Historic Stats')
+plt.ylabel('Round_Points')
+plt.show()
+
 # TODO
-# For each played fixture, find recent scores and see correlation with current fixture score
 # For each played fixture, find fixture difficulty and see correlation with current fixture score
 # Segregate data into positions and teams? Plot Data
 # Modify optimiser score metric to take into account new metrics to predict upcoming performance
